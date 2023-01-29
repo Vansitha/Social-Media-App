f2cd48b3ff7e670c930ec3113607088cbffea7c4
Vansitha Ratnayake <vansitha1@gmail.com>
2023-01-29 20:58:12 +0530

    Add functionality to create a post and hit the firestore database to save the data.

--- src/pages/create_post/CreatePost.jsx
+++ src/pages/create_post/CreatePost.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateForm() {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
   const schema = yup.object().shape({
     title: yup.string().required("Title is required"),
     description: yup.string().required("Description is required"),
@@ -17,8 +24,18 @@ export default function CreateForm() {
     resolver: yupResolver(schema),
   });
 
-  function onCreatePost(data) {
-    console.log(data);
+  const postsRef = collection(db, "posts");
+
+  async function onCreatePost(data) {
+    addDoc(postsRef, {
+      /* You can also just do ...data here to include everything because its the same thing */
+      title: data.title,
+      description: data.description,
+      username: user.displayName,
+      userId: user?.uid,
+    });
+
+    navigate("/");
   }
 
   return (
