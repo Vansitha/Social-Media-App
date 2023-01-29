41244e6931a0839c8b86cd3937f82e6bdfa5a623
Vansitha Ratnayake <vansitha1@gmail.com>
2023-01-29 08:47:29 +0530

    Setup routing for main and login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/' element={<Main />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
